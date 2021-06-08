dea517ecd403b59f8346c427ae65cca9a82626bf
lauragonzdc <lgonzdc@gmail.com>
2021-06-08 16:32:04 +0200

    hateShieldExtension

---
+++ src/content_scripts/emojiMap.js
@@ -0,0 +1,307 @@
+/*
+ * This file contains the Map of word --> emoji substitutions.
+ */
+
+let dictionary = new Map();
+dictionary.set("allumeuse", "🌸");
+dictionary.set("babtou", "🌸");
+dictionary.set("bamboula", "🌸");
+dictionary.set("bâtard", "🌸");
+dictionary.set("bâtard noir", "🌸");
+dictionary.set("bécasse", "🌸");
+dictionary.set("bimbo", "🌸");
+dictionary.set("blondasse", "🌸");
+dictionary.set("bobonne", "🌸");
+dictionary.set("bonasse", "🌸");
+dictionary.set("boniche", "🌸");
+dictionary.set("boucaque", "🌸");
+dictionary.set("bouffeur de chiens", "🌸");
+dictionary.set("bouffeuse de chiens", "🌸");
+dictionary.set("bougnoule", "🌸");
+dictionary.set("cagole", "🌸");
+dictionary.set("catin", "🌸");
+dictionary.set("chagnasse", "🌸");
+dictionary.set("chaudasse", "🌸");
+dictionary.set("chinetoque", "🌸");
+dictionary.set("ching chong", "🌸");
+dictionary.set("connard", "🌸");
+dictionary.set("connasse", "🌸");
+dictionary.set("crouille", "🌸");
+dictionary.set("débile", "🌸");
+dictionary.set("donzelle", "🌸");
+dictionary.set("enculé", "🌸");
+dictionary.set("enculer", "🌸");
+dictionary.set("fais pas ta meuf", "🌸");
+dictionary.set("fatma", "🌸");
+dictionary.set("FDP", "🌸");
+dictionary.set("fille de joie", "🌸");
+dictionary.set("fille facile", "🌸");
+dictionary.set("fille légère", "🌸");
+dictionary.set("fils de pute", "🌸");
+dictionary.set("fils de putain", "🌸");
+dictionary.set("fiotte", "🌸");
+dictionary.set("garce", "🌸");
+dictionary.set("gogole", "🌸");
+dictionary.set("gonzesse", "🌸");
+dictionary.set("gouine", "🌸");
+dictionary.set("grognasse", "🌸");
+dictionary.set("grosse vache", "🌸");
+dictionary.set("hystérique", "🌸");
+dictionary.set("macaque", "🌸");
+dictionary.set("mal-baisé", "🌸");
+dictionary.set("mal-baisée", "🌸");
+dictionary.set("mégère", "🌸");
+dictionary.set("nègre", "🌸");
+dictionary.set("négresse", "🌸");
+dictionary.set("négro", "🌸");
+dictionary.set("niakoué", "🌸");
+dictionary.set("niakouée", "🌸");
+dictionary.set("niaqué", "🌸");
+dictionary.set("niaquée", "🌸");
+dictionary.set("niaquoué", "🌸");
+dictionary.set("niaquouée", "🌸");
+dictionary.set("nigga", "🌸");
+dictionary.set("nigger", "🌸");
+dictionary.set("nique", "🌸");
+dictionary.set("nique ta mère", "🌸");
+dictionary.set("peau rouge", "🌸");
+dictionary.set("pédale", "🌸");
+dictionary.set("pédé", "🌸");
+dictionary.set("PD", "🌸");
+dictionary.set("pétasse", "🌸");
+dictionary.set("pimbêche", "🌸");
+dictionary.set("pouffiasse", "🌸");
+dictionary.set("pouffiasse", "🌸");
+dictionary.set("putain", "🌸");
+dictionary.set("putasse", "🌸");
+dictionary.set("pute", "🌸");
+dictionary.set("racoleuse", "🌸");
+dictionary.set("retourne dans ton pays", "🌸");
+dictionary.set("sale arabe", "🌸");
+dictionary.set("sale bridé", "🌸");
+dictionary.set("sale juif", "🌸");
+dictionary.set("sale juive", "🌸");
+dictionary.set("sale musulman", "🌸");
+dictionary.set("sale musulmane", "🌸");
+dictionary.set("sale noir", "🌸");
+dictionary.set("sale trans", "🌸");
+dictionary.set("salope", "🌸");
+dictionary.set("singe noir", "🌸");
+dictionary.set("tafiole", "🌸");
+dictionary.set("tantouze", "🌸");
+dictionary.set("tapette", "🌸");
+dictionary.set("tapineuse", "🌸");
+dictionary.set("tarlouse", "🌸");
+dictionary.set("tchoin", "🌸");
+dictionary.set("teubé", "🌸");
+dictionary.set("toubab", "🌸");
+dictionary.set("trainée", "🌸");
+dictionary.set("travelo", "🌸");
+dictionary.set("va manger du chien", "🌸");
+dictionary.set("vieille-peau", "🌸");
+dictionary.set("youpin", "🌸");
+dictionary.set("youpine", "🌸");
+
+let dictionaryIncl = new Map();
+dictionaryIncl.set("de nombreux", "de multiples");
+dictionaryIncl.set("de nombreuses", "de multiples");
+dictionaryIncl.set("les droits de l'Homme", "les droits humains");
+dictionaryIncl.set("homme politique", "personnalité politique");
+dictionaryIncl.set("celui qui", "quiconque");
+dictionaryIncl.set("Madame le préfet", "Madame la préfète");
+dictionaryIncl.set("Madame le directeur", "Madame la directrice");
+dictionaryIncl.set("Madame le sénateur", "Madame la sénatrice");
+dictionaryIncl.set("Madame le maire", "Madame la maire");
+dictionaryIncl.set("femme de ménage", "personnel d'entretien");
+dictionaryIncl.set("mademoiselle", "madame");
+dictionaryIncl.set("il", "iel");
+dictionaryIncl.set("elle", "iel");
+dictionaryIncl.set("ils", "iels");
+dictionaryIncl.set("elles", "iels");
+dictionaryIncl.set("les sénateurs", "les membres du sénat");
+dictionaryIncl.set("les parlementaires", "les membres du parlement");
+dictionaryIncl.set("à la satisfaction de tous", "à la satisfaction générale");
+dictionaryIncl.set("a été violée", "a été victime d'un viol");
+dictionaryIncl.set("abus sexuel", "agression sexuelle");
+dictionaryIncl.set("attouchement sexuel", "agression sexuelle");
+dictionaryIncl.set("pour tous", "pour tout le monde");
+dictionaryIncl.set("à tous", "à tout le monde");
+dictionaryIncl.set("acheteur", "acheteur.euse");
+dictionaryIncl.set("acteur", "acteur.rice");
+dictionaryIncl.set("adhérent", "adhérent.e");
+dictionaryIncl.set("adjoint", "adjoint.e");
+dictionaryIncl.set("administrateur", "administrateur.rice");
+dictionaryIncl.set("administré", "administré.e");
+dictionaryIncl.set("agent", "agent.e");
+dictionaryIncl.set("agriculteur", "agriculteur.rice");
+dictionaryIncl.set("ambassadeur", "ambassadeur.rice");
+dictionaryIncl.set("animateur", "animateur.rice");
+dictionaryIncl.set("apprenti", "apprenti.e");
+dictionaryIncl.set("artisan", "artisan.e");
+dictionaryIncl.set("assistant", "assistant.e");
+dictionaryIncl.set("attaché", "attaché.e");
+dictionaryIncl.set("auteur", "auteur.rice");
+dictionaryIncl.set("avocat", "avocat.e");
+dictionaryIncl.set("banquier", "banquier.ère");
+dictionaryIncl.set("chargé", "chargé.e");
+dictionaryIncl.set("chef", "chef.fe");
+dictionaryIncl.set("chercheur", "chercheur.e");
+dictionaryIncl.set("chroniqueur", "chroniqueur.euse");
+dictionaryIncl.set("citoyen", "citoyen.ne");
+dictionaryIncl.set("commis", "commis.e");
+dictionaryIncl.set("conseiller", "conseiller.ère");
+dictionaryIncl.set("consommateur", "consommateur.rice");
+dictionaryIncl.set("consultant", "consultant.e");
+dictionaryIncl.set("délégué", "délégué.e");
+dictionaryIncl.set("député", "député.e");
+dictionaryIncl.set("diplômé", "diplômé.e");
+dictionaryIncl.set("directeur", "directeur.rice");
+dictionaryIncl.set("écolier", "écolier.ère");
+dictionaryIncl.set("éducateur", "éducateur.rice");
+dictionaryIncl.set("électeur", "électeur.rice");
+dictionaryIncl.set("élu", "élu.e");
+dictionaryIncl.set("entraineur", "entraineur.euse");
+dictionaryIncl.set("entrepreneur", "entrepreneur.e");
+dictionaryIncl.set("étudiant", "étudiant.e");
+dictionaryIncl.set("footballeur", "footballeur.euse");
+dictionaryIncl.set("gardien", "gardien.ne");
+dictionaryIncl.set("gouverneur", "gouverneur.e");
+dictionaryIncl.set("habitant", "habitant.e");
+dictionaryIncl.set("hospitalier", "hospitalier.ère");
+dictionaryIncl.set("ingénieur", "ingénieur.e");
+dictionaryIncl.set("inspecteur", "inspecteur.rice");
+dictionaryIncl.set("instituteur", "instituteur.rice");
+dictionaryIncl.set("intellectuel", "intellectuel.le");
+dictionaryIncl.set("lycéen", "lycéen.ne");
+dictionaryIncl.set("médiateur", "médiateur.rice");
+dictionaryIncl.set("modérateur", "modérateur.rice");
+dictionaryIncl.set("officier", "officier.ère");
+dictionaryIncl.set("policier", "policier.ère");
+dictionaryIncl.set("président", "président.e");
+dictionaryIncl.set("professeur", "professeur.e");
+dictionaryIncl.set("professionnel", "professionnel.le");
+dictionaryIncl.set("programmeur", "programmeur.euse");
+dictionaryIncl.set("recteur", "recteur.rice");
+dictionaryIncl.set("remplaçant", "remplaçant.e");
+dictionaryIncl.set("retraité", "retraité.e");
+dictionaryIncl.set("sénateur", "sénateur.rice");
+dictionaryIncl.set("sénior", "sénior.e");
+dictionaryIncl.set("sportif", "sportif.ve");
+dictionaryIncl.set("suppléant", "suppléant.e");
+dictionaryIncl.set("technicien", "technicien.ne");
+dictionaryIncl.set("territorial", "territorial.e");
+dictionaryIncl.set("transporteur", "transporteur.euse");
+dictionaryIncl.set("travailleur", "travailleur.euse");
+dictionaryIncl.set("usager", "usager.ère");
+dictionaryIncl.set("développeur", "développeur.euse");
+
+browser.runtime.onMessage.addListener(addToDictionary);
+//FONCTION QUI REMPLACE LES CARACTERES ACCENTUÉS :
+String.prototype.sansAccents = function () {
+  return this.replace(/[ùûü]/g, "u")
+    .replace(/[îï]/g, "i")
+    .replace(/[àâä]/g, "a")
+    .replace(/[ôö]/g, "o")
+    .replace(/[éèêë]/g, "e")
+    .replace(/ç/g, "c");
+};
+
+let regexsEmo = new Map();
+
+for (let element of dictionary.keys()) {
+  regexsEmo.set(
+    element,
+    new RegExp("\\b" + element.sansAccents() + "\\b", "gi")
+  );
+}
+
+function addToDictionary(request) {
+  if (request.word) {
+    dictionary.set(request.word, "🌸");
+    regexsEmo.set(
+      request.word,
+      //new RegExp("^" + request.color + "$" + "[:space:]*", "gi")
+      new RegExp("\\b" + request.word + "\\b", "gi")
+    );
+    replaceText(document.body);
+  }
+}
+
+let regexsInc = new Map();
+for (let element of dictionaryIncl.keys()) {
+  regexsInc.set(element, new RegExp("\\b" + element + "\\b", "gi"));
+}
+
+function replaceText(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.parentNode && node.parentNode.nodeName === "TEXTAREA") {
+      return;
+    }
+
+    let content = node.textContent;
+    console.log(document);
+    for (let [element, emoji] of dictionary) {
+      let regex = regexsEmo.get(element);
+
+      if (!document.getElementById("badWordsList")) {
+        content = content.sansAccents().replace(regex, emoji);
+      }
+    }
+
+    node.textContent = content;
+  } else {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      replaceText(node.childNodes[i]);
+    }
+  }
+}
+
+replaceText(document.body);
+
+//FAIRE DES COOKIES
+// var checkBox = browser.windows.getAll();
+// console.log(checkBox);
+// if (checkBox.checked == true) {
+//   replaceText(document.body);
+// }
+
+function replaceTextIncl(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.parentNode && node.parentNode.nodeName === "TEXTAREA") {
+      //console.log("replaceTextIncl");
+    }
+
+    //console.log("replaceTextIncl2");
+
+    let content = node.textContent;
+
+    for (let [element, inclWord] of dictionaryIncl) {
+      let regex = regexsInc.get(element);
+
+      content = content.replace(regex, inclWord);
+    }
+
+    node.textContent = content;
+  } else {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      replaceTextIncl(node.childNodes[i]);
+    }
+  }
+}
+
+replaceTextIncl(document.body);
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+      for (let i = 0; i < mutation.addedNodes.length; i++) {
+        const newNode = mutation.addedNodes[i];
+        replaceText(newNode);
+      }
+    }
+  });
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
---
+++ src/popup/popup.js
@@ -0,0 +1,121 @@
+/* initialise variables theme & cursor */
+
+var cursorBtns = document.querySelectorAll(".cursor-container button");
+var themeBtns = document.querySelectorAll(".theme-container button");
+var wordPick = document.getElementById("word");
+var reset = document.querySelector(".reset");
+var cookieVal = { image: "", word: "", theme: "" };
+var settings = document.querySelector(".settings");
+var onOffswitch = document.querySelector (".onoffswitch");
+var onOffswitch2 = document.querySelector (".onoffswitch2");
+
+function getActiveTab() {
+  return browser.tabs.query({ active: true, currentWindow: true });
+}
+
+function removeReplaceText (){
+  if (onoffswitch == false){
+    localStorage.clear(replaceText)
+  } else return 
+}
+
+removeReplaceText();
+
+/* add listener so that when clicked, button applies background to page HTML */
+
+for (var i = 0; i < cursorBtns.length; i++) {
+  var imgName = cursorBtns[i].getAttribute("class");
+  var cursorImg = "url('images/" + imgName + ".png')";
+  cursorBtns[i].style.backgroundImage = cursorImg;
+  cursorBtns[i].style.backgroundRepeat = "no-repeat";
+
+  cursorBtns[i].onclick = function (e) {
+    getActiveTab().then((tabs) => {
+      var imgName = e.target.getAttribute("class");
+      var fullURL = browser.extension.getURL(
+        "popup/images/" + imgName + ".png"
+      );
+      browser.tabs.sendMessage(tabs[0].id, { image: fullURL });
+
+      cookieVal.image = fullURL;
+      browser.cookies.set({
+        url: tabs[0].url,
+        name: "appearancePicker",
+        value: JSON.stringify(cookieVal),
+      });
+    });
+  };
+}
+
+/* apply theme to buttons */
+/* add listener so that when clicked, button applies theme to page HTML */
+
+// for (var i = 0; i < themeBtns.length; i++) {
+//   var themeName = themeBtns[i].getAttribute("class");
+//   var themeImg = "url('images/" + themeName + ".png')";
+//   themeBtns[i].style.backgroundImage = themeImg;
+
+//   themeBtns[i].onclick = function (e) {
+//     getActiveTab().then((tabs) => {
+//       var themeName = e.target.getAttribute("class");
+//       var themeURL = browser.extension.getURL(
+//         "popup/images/" + themeName + ".png"
+//       );
+//       browser.tabs.sendMessage(tabs[0].id, { tab: themeURL });
+
+//       cookieVal.tab = themeURL;
+//       browser.cookies.set({
+//         url: tabs[0].url,
+//         name: "appearancePicker",
+//         value: JSON.stringify(cookieVal),
+//       });
+//     });
+//   };
+// }
+
+/* apply chosen word to HTML background */
+
+wordPick.onchange = function (e) {
+  getActiveTab().then((tabs) => {
+    var currWord = e.target.value;
+    browser.tabs.sendMessage(tabs[0].id, { word: currWord });
+
+    cookieVal.word = currWord;
+    browser.cookies.set({
+      url: tabs[0].url,
+      name: "appearancePicker",
+      value: JSON.stringify(cookieVal),
+    });
+  });
+};
+
+/* reset background */
+
+reset.onclick = function () {
+  getActiveTab().then((tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, { reset: true });
+
+    cookieVal = { image: "", word: "" };
+    browser.cookies.remove({
+      url: tabs[0].url,
+      name: "appearancePicker",
+    });
+  });
+};
+
+/* Report cookie changes to the console */
+
+// browser.cookies.onChanged.addListener((changeInfo) => {
+//   console.log(`Cookie changed:\n
+//               * Cookie: ${JSON.stringify(changeInfo.cookie)}\n
+//               * Cause: ${changeInfo.cause}\n
+//               * Removed: ${changeInfo.removed}`);
+// });
+
+$("#choose-answer").on("change", function () {
+  var value = `<input value="${$(this).val()}" id="selVal" />`;
+  $(value).insertAfter("#choose-answer");
+  $("#selVal").select();
+  document.execCommand("Copy");
+  $("body").find("#selVal").remove();
+});
